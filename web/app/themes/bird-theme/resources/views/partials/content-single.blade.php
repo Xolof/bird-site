cc2e26c63fae59cdb01b24a0591fbe91b4f51d47
Xolof <olaf.sebastian.johansson@gmail.com>
2023-01-28 20:27:31 +0100

    Add links to next and previous posts.

--- web/app/themes/bird-theme/resources/views/partials/content-single.blade.php
+++ web/app/themes/bird-theme/resources/views/partials/content-single.blade.php
@@ -1,6 +1,6 @@
 <article @php(post_class())>
   <header>
-    <h1 class="entry-title">
+    <h1 class="entry-title text-4xl pb-6">
       {!! $title !!}
     </h1>
 
@@ -11,9 +11,16 @@
     @php(the_content())
   </div>
 
-  <footer>
-    {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!}
-  </footer>
+  <footer class="next-prev-links">
+    <!-- {!! wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']) !!} -->
+
+    <div class="prev-link">
+      <?php previous_post_link('%link', '%title'); ?>
+    </div>
+    <div class="next-link">
+      <?php next_post_link('%link', '%title'); ?>
+    </div>
+    </footer>
 
   @php(comments_template())
 </article>
