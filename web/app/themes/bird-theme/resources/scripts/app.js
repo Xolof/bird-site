e80027b358d2a54fe02c5c70b755694de7d19c2b
Xolof <olaf.sebastian.johansson@gmail.com>
2023-01-28 20:02:33 +0100

    Improve script to validate commentform. Add script for mobile menu.

--- web/app/themes/bird-theme/resources/scripts/app.js
+++ web/app/themes/bird-theme/resources/scripts/app.js
@@ -4,7 +4,23 @@ import domReady from '@roots/sage/client/dom-ready';
  * Application entrypoint
  */
 domReady(async () => {
-  document.getElementById('commentform').removeAttribute('novalidate');
+  const commentForm = document.getElementById('commentform');
+
+  if (commentForm) {
+    commentForm.removeAttribute('novalidate');
+  }
+
+  const hamburger = document.getElementById('hamburger');
+  const nav = document.querySelector('.nav-primary');
+
+  hamburger.addEventListener('click', () => {
+    nav.classList.add('visible');
+  });
+
+  const closeButton = document.querySelector('.navCloseButton');
+  closeButton.addEventListener('click', () => {
+    nav.classList.remove('visible');
+  });
 });
 
 /**
