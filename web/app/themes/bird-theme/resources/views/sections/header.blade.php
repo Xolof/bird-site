444d51dfc6f9cc5f30cf23ef33c2633206d53763
Xolof <olaf.sebastian.johansson@gmail.com>
2023-01-28 20:32:14 +0100

    Customize header

--- web/app/themes/bird-theme/resources/views/sections/header.blade.php
+++ web/app/themes/bird-theme/resources/views/sections/header.blade.php
@@ -1,11 +1,28 @@
-<header class="banner">
+<header class="banner text-center">
+
+  <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+    $logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+  ?>
+
   <a class="brand" href="{{ home_url('/') }}">
-    {!! $siteName !!}
+    <span>{!! $siteName !!}</span>
+    <?php if ( has_custom_logo() ) {
+      echo '<img class="custom-logo" src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+    } ?>
   </a>
 
   @if (has_nav_menu('primary_navigation'))
+    <div id="hamburger">
+      <div class="bar-1"></div>
+      <div class="bar-2"></div>
+      <div class="bar-3"></div>
+    </div>
     <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+      <div class="navCloseButton">
+        <div class="bar-1"></div>
+        <div class="bar-2"></div>
+      </div>
+      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav flex', 'echo' => false]) !!}
     </nav>
   @endif
 </header>
