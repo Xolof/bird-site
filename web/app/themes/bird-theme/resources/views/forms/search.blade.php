74f29ac4af95b16b248df855f78215c6cd2714fb
Xolof <olaf.sebastian.johansson@gmail.com>
2023-01-28 20:23:49 +0100

    Make the search input required

--- web/app/themes/bird-theme/resources/views/forms/search.blade.php
+++ web/app/themes/bird-theme/resources/views/forms/search.blade.php
@@ -3,12 +3,12 @@
     <span class="sr-only">
       {{ _x('Search for:', 'label', 'sage') }}
     </span>
-
     <input
       type="search"
       placeholder="{!! esc_attr_x('Search &hellip;', 'placeholder', 'sage') !!}"
       value="{{ get_search_query() }}"
       name="s"
+      required
     >
   </label>
 
